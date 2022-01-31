20255f6314f96c6ff3c5eb335ac29037f3871629
heusyfelype <felype.heusy@gmail.com>
2022-01-31 15:09:20 -0300

    Versão final - revisada

--- scripts.js
+++ scripts.js
@@ -1,67 +1,55 @@
 
-let todosOsPratos = document.querySelectorAll(".prato_de_comida")
-let todosAsBebidas = document.querySelectorAll(".bebida")
-let todosAsSobremesas = document.querySelectorAll(".sobremesa")
+let todosOsPratos = document.querySelectorAll(".prato_de_comida");
+let todosAsBebidas = document.querySelectorAll(".bebida");
+let todosAsSobremesas = document.querySelectorAll(".sobremesa");
 
 let selecionouOPrato = false;
 let selecionouaBebida = false;
 let selecionouASobremesa = false;
-let selecionouOsTres = false;
 
-let o_prato = "";
-let valor_prato = 0.0;
-let a_bebida = "";
-let valorbebida = 0.0;
-let a_sobremesa = "";
-let valor_sobremesa = 0.0;
+let oPrato = "";
+let valorPrato = 0.0;
+let aBebida = "";
+let valorBebida = 0.0;
+let aSobremesa = "";
+let valorSobremesa = 0.0;
 
 
 function selecionar_prato(indice) {
     for (let i = 0; i < todosOsPratos.length; i++) {
         if (i != indice) {
-            todosOsPratos[i].classList.remove("borda-azul")
-            todosOsPratos[i].querySelector(".icone").classList.add("escondido")
+            todosOsPratos[i].classList.remove("borda-verde");
+            todosOsPratos[i].querySelector(".icone").classList.add("escondido");
         }
         else {
-            todosOsPratos[i].classList.toggle("borda-azul")
-            todosOsPratos[i].querySelector(".icone").classList.toggle("escondido")
+            todosOsPratos[i].classList.toggle("borda-verde");
+            todosOsPratos[i].querySelector(".icone").classList.toggle("escondido");
             
-            if (todosOsPratos[i].classList.contains("borda-azul") == true) {
+            if (todosOsPratos[i].classList.contains("borda-verde") == true) {
                 selecionouOPrato = true;
-                o_prato = todosOsPratos[i].querySelector("h3").innerText
-                valor_prato = parseFloat(todosOsPratos[i].querySelector("h4").innerText.slice(2).replace(',', '.'))
-
-                //oPratoPedido(o_prato)
-                //valorDoPratoPedido(valor_prato)
-                //console.log(o_prato)
-                //console.log(valor_prato)
-
+                oPrato = todosOsPratos[i].querySelector("h3").innerText;
+                valorPrato = parseFloat(todosOsPratos[i].querySelector("h4").innerText.slice(2).replace(',', '.'));
             } else {
                 selecionouOPrato = false;
             }
-
         }
     }
-
     continuar()
 }
 
 function selecionar_bebida(indice) {
     for (let i = 0; i < todosAsBebidas.length; i++) {
         if (i != indice) {
-            todosAsBebidas[i].classList.remove("borda-azul")
-            todosAsBebidas[i].querySelector(".icone").classList.add("escondido")
+            todosAsBebidas[i].classList.remove("borda-verde");
+            todosAsBebidas[i].querySelector(".icone").classList.add("escondido");
         }
         else {
-            todosAsBebidas[i].classList.toggle("borda-azul")
-            todosAsBebidas[i].querySelector(".icone").classList.toggle("escondido")
-            if (todosAsBebidas[i].classList.contains("borda-azul")) {
+            todosAsBebidas[i].classList.toggle("borda-verde");
+            todosAsBebidas[i].querySelector(".icone").classList.toggle("escondido");
+            if (todosAsBebidas[i].classList.contains("borda-verde")) {
                 selecionouaBebida = true;
-
-                a_bebida = todosAsBebidas[i].querySelector("h3").innerText
-                valorbebida = parseFloat(todosAsBebidas[i].querySelector("h4").innerText.slice(2).replace(',', '.'))
-                //console.log(a_bebida)
-                //console.log(valorbebida)
+                aBebida = todosAsBebidas[i].querySelector("h3").innerText;
+                valorBebida = parseFloat(todosAsBebidas[i].querySelector("h4").innerText.slice(2).replace(',', '.'));
             } else {
                 selecionouaBebida = false;
             }
@@ -73,20 +61,16 @@ function selecionar_bebida(indice) {
 function selecionar_sobremesa(indice) {
     for (let i = 0; i < todosAsSobremesas.length; i++) {
         if (i != indice) {
-            todosAsSobremesas[i].classList.remove("borda-azul")
-            todosAsSobremesas[i].querySelector("ion-icon").classList.add("escondido")
+            todosAsSobremesas[i].classList.remove("borda-verde");
+            todosAsSobremesas[i].querySelector("ion-icon").classList.add("escondido");
         }
         else {
-            todosAsSobremesas[i].classList.toggle("borda-azul")
-            todosAsSobremesas[i].querySelector(".icone").classList.toggle("escondido")
-            if (todosAsSobremesas[i].classList.contains("borda-azul")) {
+            todosAsSobremesas[i].classList.toggle("borda-verde");
+            todosAsSobremesas[i].querySelector(".icone").classList.toggle("escondido");
+            if (todosAsSobremesas[i].classList.contains("borda-verde")) {
                 selecionouASobremesa = true;
-                a_sobremesa = todosAsSobremesas[i].querySelector("h3").innerText
-                valor_sobremesa = parseFloat(todosAsSobremesas[i].querySelector("h4").innerText.slice(2).replace(',', '.'))
-                //console.log(a_sobremesa)
-                //console.log(valor_sobremesa)
-
-
+                aSobremesa = todosAsSobremesas[i].querySelector("h3").innerText;
+                valorSobremesa = parseFloat(todosAsSobremesas[i].querySelector("h4").innerText.slice(2).replace(',', '.'));
             } else {
                 selecionouASobremesa = false;
             }
@@ -97,21 +81,13 @@ function selecionar_sobremesa(indice) {
 
 
 function continuar() {
-
     if (selecionouOPrato === false || selecionouaBebida === false || selecionouASobremesa === false) {
         let fundoClicar = document.querySelector("footer > div");
         if (fundoClicar.classList.contains("comprar-verde") == true) {
-            fundoClicar.classList.remove("comprar-verde")
+            fundoClicar.classList.remove("comprar-verde");
         } if (fundoClicar.classList.contains("falta-selecionar-item") == false) {
-            fundoClicar.classList.add("falta-selecionar-item")
+            fundoClicar.classList.add("falta-selecionar-item");
         }
-        /* let clicarEmComprar = function () {
-            let elemento = document.querySelector("footer > div");
-            alert("Selecione o(s) item(ns) faltante(s)");
-        }
-
-        let botao = document.querySelector("footer > div")
-        botao.onclick = clicarEmComprar; */
     }
 
     if (selecionouOPrato === true && selecionouaBebida === true && selecionouASobremesa === true) {
@@ -127,49 +103,41 @@ function continuar() {
         }
 
         let clicarEmComprar = function () {
-            //let elemento = document.querySelector("footer > div");
-            let nome = null;
-            let endereco = null;
-            while (nome == null) {
-                nome = prompt("Qual o seu nome?");
-            }
-            while (endereco == null){
-                endereco = prompt("Qual o seu endereço?")
-            }
+            let nome = prompt("Qual o seu nome?");
+            let endereco = prompt("Qual o seu endereço?");
+            //Aqui poderiam ser colocadas algumas condicionais para verificar se o campo nome e endereço foram preenchidos, e caso não, solicitar novamente e impedir a continuidade, mas por enquanto vou deixar sem!
+
+            document.querySelector("body").classList.add("body__esconder-scroll");
 
             document.querySelector(".confirmacao-finalizar-pedido-background").classList.remove("escondido");
 
-            
-            document.querySelector("#prato").innerHTML = o_prato;
-            document.querySelector("#bebida").innerHTML = a_bebida;
-            document.querySelector("#sobremesa").innerHTML = a_sobremesa;
-            
-            document.querySelector("#valor-prato").innerHTML = parseFloat(valor_prato).toFixed(2).replace('.',',');
-            document.querySelector("#valor-bebida").innerHTML = parseFloat(valorbebida).toFixed(2).replace('.',',');
-            document.querySelector("#valor-sobremesa").innerHTML = parseFloat(valor_sobremesa).toFixed(2).replace('.',',');
+            document.querySelector("#prato").innerHTML = oPrato;
+            document.querySelector("#bebida").innerHTML = aBebida;
+            document.querySelector("#sobremesa").innerHTML = aSobremesa;
 
-            document.querySelector("#valor-total").innerHTML = 'R$ ' + parseFloat(valor_prato + valorbebida + valor_sobremesa).toFixed(2).replace('.',',');
+            document.querySelector("#valor-prato").innerHTML = parseFloat(valorPrato).toFixed(2).replace('.',',');
+            document.querySelector("#valor-bebida").innerHTML = parseFloat(valorBebida).toFixed(2).replace('.',',');
+            document.querySelector("#valor-sobremesa").innerHTML = parseFloat(valorSobremesa).toFixed(2).replace('.',',');
 
-            let texto_whats = `Olá, gostaria de fazer o pedido: \n\n - Prato:  ${o_prato}\n - Bebida: ${a_bebida}\n - Sobremesa: ${a_sobremesa}\n\n Total: R$ ${parseFloat(valor_prato + valorbebida + valor_sobremesa).toFixed(2).replace('.',',')} \n\n Nome: ${nome} \n Endereço: ${endereco}`;
-                texto_whats = window.encodeURIComponent(texto_whats);
+            document.querySelector("#valor-total").innerHTML = 'R$ ' + parseFloat(valorPrato + valorBebida + valorSobremesa).toFixed(2).replace('.',',');
 
-            let celular = 5547984280676
+            let textoWhats = `Olá, gostaria de fazer o pedido: \n\n - Prato:  ${oPrato}\n - Bebida: ${aBebida}\n - Sobremesa: ${aSobremesa}\n\n Total: R$ ${parseFloat(valorPrato + valorBebida + valorSobremesa).toFixed(2).replace('.',',')} \n\n Nome: ${nome} \n Endereço: ${endereco}`;
+                textoWhats = window.encodeURIComponent(textoWhats);
+            let celular = 5547984280676;
 
             let boraProWhatsapp = function(){
-                window.open("https://wa.me/" + celular + "?text=" + texto_whats)
+                window.open("https://wa.me/" + celular + "?text=" + textoWhats);
             }
-
-            let tudoCerto = document.querySelector("#bora-pro-whats");
-            tudoCerto.onclick = boraProWhatsapp;
+            document.querySelector("#bora-pro-whats").onclick = boraProWhatsapp;
 
             let voltarProPedido = function(){
                 document.querySelector(".confirmacao-finalizar-pedido-background").classList.add("escondido");
+                document.querySelector("body").classList.remove("body__esconder-scroll");
             }
+            document.querySelector("#retornar-ao-pedido").onclick = voltarProPedido;
+            document.querySelector(".confirmacao-finalizar-pedido-background").onclick = voltarProPedido;
 
-            let voltar = document.querySelector("#retornar-ao-pedido");
-            voltar.onclick = voltarProPedido;
         }
-        let botao = document.querySelector("footer > div");
-        botao.onclick = clicarEmComprar;
+        document.querySelector("footer > div").onclick = clicarEmComprar;
     }
 }
